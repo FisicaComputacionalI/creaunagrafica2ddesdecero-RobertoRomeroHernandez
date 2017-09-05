4154e3bcbfe38fe7d84b7147a9fbbda0ba064d77
RobertoRomeroHernandez <31378324+RobertoRomeroHernandez@users.noreply.github.com>
2017-09-05 10:28:39 -0500

    Tarea 2

---
+++ temp2.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pylab as pl
+x = [  1, 2,  3, 4, 5, 6]
+y = [7.5,10,8.6, 8, 8, 8]
+pl.title (‘Promedio por semestre’)
+pl.ylabel(‘Promedio’)
+pl.xlabel(‘Semestre’)
+pl.axis([1,6,5,10])
+pl.plot (x,y,'k--', linewidth = 2)
+pl.savefig('temp2.png')
+pl.show()
